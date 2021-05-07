cb741023d905e93d97971c0ad606408875052df7
mavro <mrojasb2000@gmail.com>
2021-05-07 18:11:17 -0400

    Added number variables

---
+++ datatypes/main.dart
@@ -0,0 +1,24 @@
+main() {
+  // numbers
+  int a = 10;
+  double b = 10;
+
+  int x = 10, y = 20, z = 30;
+
+  print(x);
+  print(y);
+  print(z);
+
+  int _a = 30;
+  print(_a);
+
+  double $b = 45.55;
+  print($b);
+  // string
+
+  // Boolean
+
+  // Lists - Slice
+
+  // Sets
+}
